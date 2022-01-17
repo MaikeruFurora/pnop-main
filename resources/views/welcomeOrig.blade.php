ee346963f68727d6d5a36a823d5af07db4b1b8b1
MaikeruFurora <you41727@gmail.com>
2022-01-17 13:02:48 +0700

    test

--- resources/views/welcomeOrig.blade.php
+++ resources/views/welcomeOrig.blade.php
@@ -124,7 +124,7 @@
                 <div class="col-12 col-md-5 col-lg-6 order-md-2 text-center">
                     <!-- Image -->
                     <figure class="w-100">
-                        <img alt="Image placeholder" src="{{ asset('assets/img/svg/illustrations/illustration-4.svg') }}" class="img-fluid mw-md-120">
+                        <img alt="Image placeholder" src="{{ asset('assets/img/svg/illustrations/illustration-1.svg') }}" class="img-fluid mw-md-120">
                     </figure>
                 </div>
                 <div class="col-12 col-md-7 col-lg-6 order-md-1 pr-md-5">
